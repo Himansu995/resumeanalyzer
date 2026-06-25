d80976402181242b5b2063c9354ae72afa49177a
himansu <himansusheoran2006@gamil.com>
2026-06-25 09:40:09 +0530

    Added resume dashboard with Thymeleaf

--- src/main/java/com/himansu/resumeanalyzer/controller/HomeController.java
+++ src/main/java/com/himansu/resumeanalyzer/controller/HomeController.java
@@ -1,14 +1,31 @@
 package com.himansu.resumeanalyzer.controller;
 
+import com.himansu.resumeanalyzer.service.ResumeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
+    private final ResumeService resumeService;
+
+    public HomeController(ResumeService resumeService)
+    {
+        this.resumeService = resumeService;
+    }
+
     @GetMapping("/")
     public String home()
     {
         return "index";
+    }
+
+    @GetMapping("/resumes")
+    public String showResumes(Model model)
+    {
+        model.addAttribute("resumes", resumeService.getAllResumes());
+
+        return "resumes";
     }
 }
